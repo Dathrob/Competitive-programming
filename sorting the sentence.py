9f8782dfe285aafdf03c528ccd423d198540ec5b
Misganaw Meseret <dev.misganaw.meseret@gmail.com>
2022-12-16 23:21:20 +0300

    sorting the sentence

--- sorting the sentence.py
+++ sorting the sentence.py
@@ -1,20 +1,22 @@
-#40 min
-def sortSentence(str):
-    sliced = str.split()
-    new_hash = dict()
-    count = -1
-    new_sentence = []
-    sentence = ""
-    for i in sliced:
-        count+=1 
-        for j in i:
-            if j.isnumeric():
-                j = int(j)
-                word = sliced[count]
-                new_hash[j] =  word[:-1]
-    for i in range(1,len(sliced)+1):
-        str = "".join([new_hash[i]])
-        sentence = sentence + str + " " 
-    print(sentence)
-sortSentence("is2 sentence4 This1 a3")
-    
+class Solution(object):
+    def sortSentence(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+        sliced = s.split()
+        new_hash = dict()
+        count = -1
+        new_sentence = []
+        sentence = ""
+        for i in sliced:
+            count+=1 
+            for j in i:
+                if j.isnumeric():
+                    j = int(j)
+                    word = sliced[count]
+                    new_hash[j] =  word[:-1]
+        for i in range(1,len(sliced)+1):
+            str = "".join([new_hash[i]])
+            sentence = sentence + str + " " 
+        return(sentence[:-1])
