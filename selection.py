76a2ea14564f0fadb52335f8a18b907fafa57a0b
Misganaw Meseret <dev.misganaw.meseret@gmail.com>
2022-12-16 23:37:44 +0300

    selection sort

--- selection.py
+++ selection.py
@@ -1,17 +1,16 @@
-#started at 22:01
-#finished at 22:11
-def select(arr):
-    smallest = arr[0]
-    smallest_index = 0
-    for i in range(1,len(arr)):
-        if arr[i] < smallest:
-            smallest = arr[i]
-            smallest_index= i
-    return smallest_index
-def selectionSort(arr):
-    sorted_array = []
-    for i in range(len(arr)):
-        smallest = select(arr)
-        sorted_array.append(arr.pop(smallest))
-    return sorted_array
-print(selectionSort([10, 9, 8, 7, 6, 5, 4, 3, 2, 1]))
+class Solution: 
+    def select(self, arr, i):
+        smallest = arr[0]
+        smallest_index = 0
+        for j in range(i,len(arr)):
+            if arr[j] < smallest:
+                smallest = arr[j]
+                smallest_index= j
+        return smallest_index
+    
+    def selectionSort(self, arr,n):
+        sorted_array = []
+        for i in range(len(arr)):
+            smallest = self.select(arr,n-1)
+            sorted_array.append(arr.pop(smallest))
+        return sorted_array
