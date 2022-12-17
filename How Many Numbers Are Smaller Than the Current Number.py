540dbd9df2a761db9544b1d79fce3d33697921f9
Misganaw Meseret <dev.misganaw.meseret@gmail.com>
2022-12-17 23:07:17 +0300

    how many numbers

--- How Many Numbers Are Smaller Than the Current Number.py
+++ How Many Numbers Are Smaller Than the Current Number.py
@@ -1,15 +1,17 @@
-#10 minutes
-def numssmaller(array):
-    smaller_list = []
-    count = 0
-    for i in range(len(array)):
-        for j in range(len(array)):
-            if i == j:
-                continue
-            elif array[i] > array[j]:
-                count += 1
-        smaller_list.append(count)
+class Solution(object):
+    def smallerNumbersThanCurrent(self, array):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        smaller_list = []
         count = 0
-    return smaller_list
-print(numssmaller([7,7,7,7]))
-
+        for i in range(len(array)):
+            for j in range(len(array)):
+                if i == j:
+                    continue
+                elif array[i] > array[j]:
+                    count += 1
+            smaller_list.append(count)
+            count = 0
+        return smaller_list
