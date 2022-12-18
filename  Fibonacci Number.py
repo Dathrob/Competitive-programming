b70da1392be463131b2238a865774bc0bf8a4f43
Misganaw Meseret <dev.misganaw.meseret@gmail.com>
2022-12-18 23:15:14 +0300

    fibonacci number

---  Fibonacci Number.py
+++  Fibonacci Number.py
@@ -1,10 +1,6 @@
-'''
-taken 25 minutes
-
-'''
-def fibonacci(num):
-    if (num <= 1):
-        return num
-    else:
-        return fibonacci(num-1) + fibonacci(num-2)
-print(fibonacci(5))
+class Solution:
+    def fib(self, num: int) -> int:
+        if (num <= 1):
+            return num
+        else:
+            return self.fib(num-1) + self.fib(num-2)
