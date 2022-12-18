e2e5390f812ef0ecbe67c4635613cac0b17fe66f
Misganaw Meseret <dev.misganaw.meseret@gmail.com>
2022-12-18 23:00:32 +0300

    power of three

--- power-of-three.py
+++ power-of-three.py
@@ -1,8 +1,8 @@
-def powerofthree(num):
-    if (((num / 3) == 1) or num == 1):
-        return True
-    elif num % 3 != 0 or num == 0:
-        return False
-    else:
-        return powerofthree(num/3)
-print(powerofthree(3486784401))
+class Solution:
+    def isPowerOfThree(self, num: int) -> bool:
+        if (((num / 3) == 1) or num == 1):
+            return True
+        elif num % 3 != 0 or num == 0:
+            return False
+        else:
+            return self.isPowerOfThree(num/3)
